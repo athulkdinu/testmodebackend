bbc52ecada9d74b8761547b632f66f0a198989ec
athulkdinu <athulkdinu@gmail.com>
2025-12-18 09:33:15 +0530

    last

--- server.js
+++ server.js
@@ -26,7 +26,13 @@ const io = new Server(server, {
     }
 });
 
-app.use(cors());
+// CORS configuration
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
